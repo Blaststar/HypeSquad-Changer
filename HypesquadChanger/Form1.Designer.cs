b926989a629fee000689462066fe420e9359d33b
Blaststarx <87101472+Blaststarx@users.noreply.github.com>
2022-04-27 20:59:29 -0500

    first commit

---
+++ HypesquadChanger/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace HypesquadChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.token = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // token
+            // 
+            this.token.Location = new System.Drawing.Point(459, 196);
+            this.token.Name = "token";
+            this.token.Size = new System.Drawing.Size(424, 23);
+            this.token.TabIndex = 0;
+            this.token.TextChanged += new System.EventHandler(this.token_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(403, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Token";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(548, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Set HypeSquad";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(575, 87);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 19);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Bravery";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(575, 112);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 19);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Brilliance";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(575, 137);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(66, 19);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Balance";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(538, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "HypeSquad Options";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1214, 620);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.token);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HypeSquad Changer - https://blaststar.github.io/Skid-Tools";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox token;
+        private Label label1;
+        private Button button1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Label label2;
+    }
+}
---
+++ HypesquadChanger/Form1.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace HypesquadChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string hype = "";
+
+        static Random rd = new Random();
+
+        internal static string cookie(int stringLength)
+        {
+            const string allowedChars = "abcdefghijklmnopqrstuvwxyz123456789";
+            char[] chars = new char[stringLength];
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                chars[i] = allowedChars[rd.Next(0, allowedChars.Length)];
+            }
+
+            return new string(chars);
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            return;
+        }
+
+        private void token_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            hype = "1";
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            hype = "2";
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            hype = "3";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user_token = token.Text;
+            string body = $"{{\"house_id\":\"{hype}\"}}";
+            if (user_token == "" || user_token == " ")
+            {
+                MessageBox.Show("A valid token must be provided!", "HypeSquad Changer", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            } else if(hype == "")
+            {
+                MessageBox.Show("A hype must be chosen!", "HypeSquad Changer", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            } else
+            {
+                try
+                {
+                    string oreo = "__cfduid=" + cookie(43) + ';' + " __dcfduid=" + cookie(32) + "; locale=en-US";
+                    var request = new HttpClient();
+                    request.DefaultRequestHeaders.Clear();
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("accept", "*/*");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("accept-language", "en-US");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("authorization", user_token);
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("content-type", "application/json");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("cookie", oreo);
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("origin", "https://discord.com");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("pragma", "no-cache");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("referer", "https://discord.com/channels/@me");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("sec-ch-ua", "\"Chromium\";v=\"94\", \"Google Chrome\";v=\"94\", \";Not A Brand\";v=\"99\"");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("sec-ch-ua-mobile", "?1");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("sec-ch-ua-platform", "\"Android\"");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-dest", "empty");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-mode", "cors");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-site", "same-origin");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Mobile Safari/537.36");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("x-debug-options", "bugReporterEnabled");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("x-discord-locale", "en-US");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("x-kl-ajax-request", "Ajax_Request");
+                    request.DefaultRequestHeaders.TryAddWithoutValidation("x-super-properties", "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9");
+                    var res = (int)request.PostAsync("https://discord.com/api/v9/hypesquad/online", new StringContent(body, Encoding.UTF8, "application/json")).Result.StatusCode;
+                    if(res == 204)
+                    {
+                        MessageBox.Show("Sucessfully set HypeSquad!", "HypeSquad Changer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    } else if(res == 401)
+                    {
+                        MessageBox.Show("An invalid token was provided!", "HypeSquad Changer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        token.Text = "";
+                        return;
+
+                    } else if(res == 403)
+                    {
+                        MessageBox.Show("The token provided is locked!", "HypeSquad Changer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        token.Text = "";
+                        return;
+
+                    } else if(res == 429)
+                    {
+                        MessageBox.Show("You are currently ratelimited!", "HypeSquad Changer", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    } else
+                    {
+                        MessageBox.Show("An error occured, please try again later.", "HypeSquad Changer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("A valid token must be provided!", "HypeSquad Changer", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
